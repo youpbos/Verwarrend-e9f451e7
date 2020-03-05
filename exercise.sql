c519d369071bd89b85aae48d30fd89b28b2b85b8
youp bos <2107418@talnet.nl>
2020-03-05 14:48:07 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,16 +1,3 @@
--- ALTER TABLE planeten
--- MODIFY diameter BIGINT NOT NULL;
--- ALTER TABLE planeten
--- MODIFY afstand_tot_de_zon BIGINT NOT NULL;
--- ALTER TABLE planeten
--- MODIFY massa BIGINT NOT NULL;
-
--- ALTER TABLE planeten ADD
--- (
--- bezoek_datum NOW() NOT NULL
--- );
-
--- UPDATE planeten 
--- SET bezoek_datum = NOW();
-
--- SET @@time_zone = "sytem";
+UPDATE planeten
+SET name = "Teenalp"
+WHERE id = 6;
